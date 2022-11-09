9dd91bdc833d6beab3285329c5aade8d9bf9d267
belunina <belunina94@mail.ru>
2022-11-09 15:26:12 +0300

    Выполнены Тесты 1-3.

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,19 @@
 public class MainClass {
 
-    private int class_number = 20;
+    //ТЕСТ1
+    public int getLocalNumber() {
+        return 14;
+    }
 
+    //ТЕСТ2
+    private int class_number = 20;
     public int getClassNumber() {
         return class_number;
+    }
+
+    //ТЕСТ3
+    private String class_string = "Hello, world";
+    public String getClassString() {
+        return class_string;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,10 +1,25 @@
+import com.sun.deploy.util.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest extends MainClass {
 
+    //ТЕСТ1
+    @Test
+    public void testGetLocalNumber() {
+        Assert.assertTrue("Метод должен вернуть 14",getLocalNumber() == 14);
+    }
+
+    //ТЕСТ2
     @Test
     public void testGetClassNumber() {
         Assert.assertTrue("getClassNumber() !> 45",getClassNumber() > 45);
+    }
+
+    //ТЕСТ3
+    @Test
+    public void testGetClassString() {
+        Assert.assertTrue("Строка содержит подстроку \"hello\" или \"Hello\"",getClassString().toLowerCase().contains("hello".toLowerCase()));
+
     }
 }
